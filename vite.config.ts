d45c21c42996c85e11a6bcae627c55dce33c6134
ysulchaum <ysulchaum@gmail.com>
2026-08-16 23:10:48 +0800

    Add GitHub Actions workflow for deploying to GitHub Pages and update site metadata

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "https://studentstudypen.com/" : "/",
   plugins: [react(), tailwindcss()],
-  base: "/",
-});
+}));
